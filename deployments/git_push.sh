297aee7fad0a507e2dadda295594a33e7ef7f896
jcolinmurray <colin@directsalesforge.com>
2015-05-20 12:15:47 -0500

    Update git_push.sh

--- deployments/git_push.sh
+++ deployments/git_push.sh
@@ -14,5 +14,16 @@ REMOTE_BRANCH=${REMOTE_BRANCH:?'You need to configure the REMOTE_BRANCH environm
 
 set -e
 
-git fetch --unshallow || true
-git push ${REMOTE_REPOSITORY} ${CI_COMMIT_ID}:${REMOTE_BRANCH}
+git config –global user.email “$GITMAIL”
+git config –global user.name “$GITUSER”
+# Clone the whole azure repository
+cd
+git clone $GITREPO azure
+# Add the compiled app
+cd azure
+rm -R -f *
+cp -R -f ~/clone/dist/* .
+git add -A
+git commit -m “Code shipped”
+# push to azure
+git push origin
